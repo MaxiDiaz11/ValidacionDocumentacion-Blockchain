465281b247bd117bad8cd294d29a9f6a9987f91a
Maxi Diaz <maxi.diaz66@gmail.com>
2022-10-20 22:07:18 -0300

    server without ipfs

--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,30 @@
-// import express from "express";
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
+
+//!IPFS
+
 // import * as IPFS from "ipfs-core";
-// const app = express();
-// const port = 3000;
 
 // const createNode = async () => {
-//   const response = await IPFS.create();
-//   return response;
+//   const node = await IPFS.create();
+
+//   const fileAdded = await node.add({
+//     path: "test.txt",
+//     content: "Hello IPFS!",
+//   });
+
+//   console.log("Added file:", fileAdded.path, fileAdded.cid);
 // };
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
 
-// app.get("/create", (req, res) => {
-//   res.send(createNode());
-// });
-
-// app.listen(port, () => {
-//   return console.log(`Express is listening at http://localhost:${port}`);
-// });
-
-import * as IPFS from "ipfs-core";
-import type { CID } from "multiformats/cid";
-
-
-const createNode = async () => {
-  const node = await IPFS.create();
-
-  const fileAdded = await node.add({
-    path: "test.txt",
-    content: "Hello IPFS!",
-  });
-
-  console.log("Added file:", fileAdded.path, fileAdded.cid);
-};
-
-// const readFile = async (ipfs: IPFS, cid: CID): Promise<string> => {
-//   const decoder = new TextDecoder();
-//   let content = "";
-
-//   for await (const chunk of ipfs.cat(cid)) {
-//     content += decoder.decode(chunk, {
-//       stream: true,
-//     });
-//   }
-
-//   return content;
-// };
-
-createNode();
+// createNode();
