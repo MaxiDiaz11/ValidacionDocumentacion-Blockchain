41ee1cc2e4b8438df85bf2bfe1d8c840dba04de8
jpsoria6 <jpsoria6@hotmail.com>
2022-10-20 22:49:59 -0300

    base solution in node

---
+++ src/app.js
@@ -0,0 +1,30 @@
+// import express from "express";
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+
+// app.listen(port, () => {
+//   return console.log(`Express is listening at http://localhost:${port}`);
+// });
+
+//!IPFS
+
+import * as IPFS from "ipfs-core";
+
+const createNode = async () => {
+  const node = await IPFS.create();
+
+  const fileAdded = await node.add({
+    path: "test.txt",
+    content: "Hello IPFS!",
+  });
+
+  console.log("Added file:", fileAdded.path, fileAdded.cid);
+};
+
+
+createNode();
