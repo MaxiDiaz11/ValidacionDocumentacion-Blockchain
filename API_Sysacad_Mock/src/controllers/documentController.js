7d359581cc8d13d6328af4bfb4b15d0324ce99a2
jpsoria6 <jpsoria6@hotmail.com>
2022-11-07 23:02:28 -0300

    rollback to pdf file

--- API_Sysacad_Mock/src/controllers/documentController.js
+++ API_Sysacad_Mock/src/controllers/documentController.js
@@ -3,18 +3,21 @@ const pdf2base64 = require('pdf-to-base64');
 
 exports.getConstanciaDeAlumnoRegular = async (req, res) => {
     try {
-        pdf2base64('./src/documents/ConstanciaDeAlumnoRegular.pdf')
-        .then(
-            (response) => {
-                console.log(response); //cGF0aC90by9maWxlLmpwZw==
-                res.send({pdfToBase64: response});
-            }
-        )
-        .catch(
-            (error) => {
-                console.log(error); //Exepection error....
-            }
-        )
+        // pdf2base64('./src/documents/ConstanciaDeAlumnoRegular.pdf')
+        // .then(
+        //     (response) => {
+        //         console.log(response); //cGF0aC90by9maWxlLmpwZw==
+        //         res.send({pdfToBase64: response});
+        //     }
+        // )
+        // .catch(
+        //     (error) => {
+        //         console.log(error); //Exepection error....
+        //     }
+        // )
+        let pdfFile = fs.readFileSync('./src/documents/ConstanciaDeAlumnoRegular.pdf')
+        res.contentType("application/pdf");
+        res.send(pdfFile);
     } catch (error) {
         console.log(error)
         res.status(500).send('Hubo un error en el servidor')
@@ -24,18 +27,21 @@ exports.getConstanciaDeAlumnoRegular = async (req, res) => {
 //actualizar tarea
 exports.getEstadoAcademico = async (req, res) => {
     try {
-        pdf2base64('./src/documents/EstadoAcadémico.pdf')
-        .then(
-            (response) => {
-                console.log(response); //cGF0aC90by9maWxlLmpwZw==
-                res.send({pdfToBase64: response});
-            }
-        )
-        .catch(
-            (error) => {
-                console.log(error); //Exepection error....
-            }
-        )
+        // pdf2base64('./src/documents/EstadoAcadémico.pdf')
+        // .then(
+        //     (response) => {
+        //         console.log(response); //cGF0aC90by9maWxlLmpwZw==
+        //         res.send({pdfToBase64: response});
+        //     }
+        // )
+        // .catch(
+        //     (error) => {
+        //         console.log(error); //Exepection error....
+        //     }
+        // )
+        let pdfFile = fs.readFileSync('./src/documents/EstadoAcadémico.pdf')
+        res.contentType("application/pdf");
+        res.send(pdfFile);
     } catch (error) {
         console.log(error)
         res.status(500).send('Hubo un error en el servidor')
